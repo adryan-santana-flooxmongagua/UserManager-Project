12fb232b423fa0a9658fcf5d3c660d0efca66c11
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-01-28 08:37:25 -0300

    refactor: add if-else conditions to improve logic

--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,14 @@ var admin = document.querySelector("#exampleInputAdmin");
 
 var fields = document.querySelectorAll("#form-user-create [name]");
 
-fields.forEach(function (field, index) {
+fields.forEach(function(field, index) {
+    if (field.name == "gender") {
+        if (field.checked) {
+            console.log("SIM", field);
+        }
+    } else {
+        console.log("NÃO");
+    }
 
-    console.log(field.name);
-
+    // console.log(field.id, field.name, field.value, field.checked, index);
 });
