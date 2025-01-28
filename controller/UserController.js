abce8186d49a3dbbf958dd6f80a1576a1ae3cd62
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-01-28 12:29:19 -0300

    refactor: improve Promise logic for better readability

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,7 +1,7 @@
 class UserController {
 
     constructor(formId, tableId) {
-        
+
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
 
@@ -9,72 +9,110 @@ class UserController {
 
     }
 
-    onSubmit(){
-        
+    onSubmit() {
+
         this.formEl.addEventListener("submit", event => {
 
             event.preventDefault();
-    
-            this.addLine(this.getValues());
 
+            let values = this.getValues();
 
+            this.getPhoto().then(
+                (content) => {
+
+                    values.photo = content;
+                    this.addLine(values);
+
+                }, 
+                (e) => {
+
+                    console.error(e);
+
+                }
+            );
         });
-
     }
 
-    getValues(){
+    getPhoto() {
+
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+                if (item.name === 'photo') {
+
+                    return item;
+
+                }
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+
+                reject(e);
+
+            };
+
+            fileReader.readAsDataURL(file);
+
+        });
+    }
+
+    getValues() {
 
         let user = {};
 
-        this.formEl.elements.forEach(function(field, index) {
+        [...this.formEl.elements].forEach(function(field, index) {
 
-            if (field.name == "gender") {
-        
+            if (field.name === "gender") {
+
                 if (field.checked) {
-        
+
                     user[field.name] = field.value;
-        
+
                 }
-        
+
             } else {
-        
+
                 user[field.name] = field.value;
-        
+
             }
         });
-    
+
         return new User(
-            user.name,
-            user.gender,
-            user.birth,
-            user.country,
-            user.email,
-            user.password,
-            user.photo,
+            user.name, 
+            user.gender, 
+            user.birth, 
+            user.country, 
+            user.email, 
+            user.password, 
+            user.photo, 
             user.admin
         );
-
-         
     }
 
-    addLine(dataUser){
+    addLine(dataUser) {
     
-     this.tableEl.innerHTML = `  
-                     <tr>
-                       <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-                       <td>${dataUser.name}</td>
-                       <td>${dataUser.email}</td>
-                       <td>${dataUser.admin}</td>
-                       <td>${dataUser.birth}</td>
-                       <td>
-                         <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                       </td>
-                     </tr>
-                      `;
-    
-                      document.getElementById("table-users").appendChild(tr);
-    
+        this.tableEl.innerHTML = `
+            <tr>
+                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${dataUser.admin}</td>
+                <td>${dataUser.birth}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr>
+        `;
     }
-    
 }
