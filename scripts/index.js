ca72ccf83a2ef9287da826e68a3f34d2541b4ec9
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-01-28 08:48:36 -0300

    feat: integrate form data into JSON object

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,22 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var gender = document.querySelector("#exampleInputGenderF");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail1");
-var password = document.querySelector("#exampleInputPassword1");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
 fields.forEach(function(field, index) {
+
     if (field.name == "gender") {
+
         if (field.checked) {
-            console.log("SIM", field);
+
+           user[field.name] = field.value;
+
         }
+
     } else {
-        console.log("NÃO");
+
+        user[field.name] = field.value;
+
     }
 
+    console.log(user);
     // console.log(field.id, field.name, field.value, field.checked, index);
 });
